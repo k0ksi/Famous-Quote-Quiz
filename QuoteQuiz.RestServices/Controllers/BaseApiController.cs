6c7f8c5646b61f56653ed371013bc22785a95ee4
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-01-14 12:52:46 +0200

    Added BaseApiController

---
+++ QuoteQuiz.RestServices/Controllers/BaseApiController.cs
@@ -0,0 +1,21 @@
+﻿namespace QuoteQuiz.RestServices.Controllers
+{
+    using System.Web.Http;
+    using Data;
+    using Data.UnitOfWork;
+
+    public class BaseApiController : ApiController
+    {
+        protected BaseApiController()
+            : this(new QuoteQuizData(new QuoteQuizDbContext()))
+        {
+        }
+
+        protected BaseApiController(IQuoteQuizData data)
+        {
+            this.Data = data;
+        }
+
+        protected IQuoteQuizData Data { get; private set; }
+    }
+}
